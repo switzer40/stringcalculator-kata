627bc8a0c720073b210b9889dd002c223e13d46f
switzer40 <switzer40@googlemail.com>
2017-02-06 15:54:51 +0100

    secobd test passes

--- stringcalculator/stringcalculator/Calculator.cs
+++ stringcalculator/stringcalculator/Calculator.cs
@@ -4,7 +4,11 @@
     {
         public int Add(string numbers)
         {
-            return 0;
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            return 1;
         }
     }
 }
