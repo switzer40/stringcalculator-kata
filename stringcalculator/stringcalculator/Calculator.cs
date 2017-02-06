f53b23fc0d7451de726a070e2aa2e2111dccc48c
switzer40 <switzer40@googlemail.com>
2017-02-06 15:26:47 +0100

    first test

---
+++ stringcalculator/stringcalculator/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringcalculator
+{
+    public class Calculator
+    {
+        public int Add(string v)
+        {
+            int result = 0;
+            if (v == "")
+            {
+                result = 0;
+            }
+            return  result;
+        }
+    }
+}
---
+++ stringcalculator/stringcalculator/CalculatorAddShould.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringcalculator
+{
+    [TestFixture]
+    public class CalculatorAddShould
+    {
+        [Test]
+        public void ReturnZeroGivenEmptyString()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add("");
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
